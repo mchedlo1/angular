f78e36e045f3feb41dfdf2c1e64d423a46b3eb1f
mchedlo1 <mdatuna22@gmail.com>
2025-04-22 13:47:01 +0400

    first commit

---
+++ src/app/main/main.component.ts
@@ -0,0 +1,44 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-main',
+  imports: [FormsModule, CommonModule],
+  templateUrl: './main.component.html',
+  styleUrl: './main.component.scss'
+})
+export class MainComponent {
+
+  inp = ""
+  inpArr : string[] = []
+
+
+  save(){
+
+    if(this.inp != ""){
+      this.inpArr.push(this.inp)
+    }
+    else{
+      console.log("please fill out the input")
+    }
+    this.inp = ""
+
+  }
+
+  print(){
+
+    
+    for(let el of this.inpArr){
+
+      console.log(el)
+
+    }
+
+  }
+}
+
+// Main კომპონენტში შემოიტანეთ ინფუთი და ორი ღილაკი. პირველ ღილაკზე კლიკით 
+// ინფუთში ჩაწერილი ტექსტი ჩაიწეროს არაიში და მეორე ღილაკზე კლიკით ეს არაის 
+// ელემენტები დაიბეჭროდ კონსოლში ცალ-ცალკე ხაზზე.
+
