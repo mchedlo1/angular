a970af582c3a2d0ba42037d835948b1db114f2d3
mchedlo1 <mdatuna22@gmail.com>
2025-05-01 21:24:52 +0400

    homework 6

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,9 +3,14 @@ import { Routes } from '@angular/router';
 import { HeaderComponent } from './header/header.component';
 import { Main1Component } from './main1/main1.component';
 import { MainComponent } from './main/main.component';
+import { ProductsComponent } from './products/products.component';
+import { ProductsDetailsComponent } from './products/products-details/products-details.component';
 
 export const routes: Routes = [
     {path: '', redirectTo: '/main', pathMatch: 'full'},
     {path: 'main', component : MainComponent},
     {path: 'main1', component : Main1Component},
+    {path: 'products', component: ProductsComponent},
+    {path: 'products/:id', component: ProductsDetailsComponent},
+    {path: 'products/:id/:category', component: ProductsDetailsComponent},
 ];
